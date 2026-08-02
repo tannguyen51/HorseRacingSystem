4e076f4e9e29f33f3ffef19729b979fdfa7ba2b2
QuangNhat121 <nhat9983@gmail.com>
2026-08-02 14:43:51 +0700

    fix logic OwnerHorse

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:5226",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const targetUrl = env.VITE_API_BASE_URL || 'http://localhost:5226';
+  
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: targetUrl,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
-  },
+  }
 })
